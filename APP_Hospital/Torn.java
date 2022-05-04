c1e9f52a9aa276d210364170547d84b5573e4d62
Nil Pujol <nilpujolporta@gmail.com>
2022-05-04 13:00:21 +0200

    Estructura temporal de torns

--- APP_Hospital/Torn.java
+++ APP_Hospital/Torn.java
@@ -1,5 +1,41 @@
 package APP_Hospital;
 
 public class Torn {
+    private int ID;
+    private String planta;
+    private Dia dia;
+    private String tipus;
+
+    public Torn(int ID, String planta, Dia dia, String tipus) {
+        this.ID = ID;
+        this.planta = planta;
+        this.dia = dia;
+        this.tipus = tipus;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public String getPlanta() {
+        return planta;
+    }
+    public Dia getDia() {
+        return dia;
+    }
+    public String getHorari() {
+        return tipus;
+    }
+    
+
+    //END setters i getters
+    
+    public boolean reservar(Treballador treballador, String posició) {
+        //comprova si pot reservar
+        return true;
+    }
+    public boolean cancelar(Treballador treballador){
+        //comprova si pot cancelar
+        return true;
+    }
     
 }

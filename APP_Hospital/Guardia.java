08acb552f692b7ae3565ffd61c7225eaaf3e9829
Nil Pujol <nilpujolporta@gmail.com>
2022-05-06 11:59:25 +0200

    improved set i get

--- APP_Hospital/Guardia.java
+++ APP_Hospital/Guardia.java
@@ -2,14 +2,17 @@ package APP_Hospital;
 
 public class Guardia {
     private int id;
-    private Treballador TPrincipal;
+    private int IDTPrincipal;
     private Torn ParentTorn;
     
     public Guardia(Torn ParentTorn) {//des de 0
         this.id = 1;
         this.ParentTorn = ParentTorn;
     }
-    public Guardia(int id, Treballador TPrincipal, Torn ParentTorn) {//desde la BD
+    public Guardia(int id, int IDTPrincipal, Torn ParentTorn) {//desde la BD
 
+    }
+    public void setTPrincipal(Treballador Treballador) {
+        this.IDTPrincipal = Treballador.getId();
     }
 }
--- APP_Hospital/Treballador.java
+++ APP_Hospital/Treballador.java
@@ -1,5 +1,44 @@
 package APP_Hospital;
 
 public class Treballador {
+    private short id;
+    private String nom;
+    private String cognoms;
+    private String carrec;
+    private short prioritat;
+
+    public Treballador(short id, String nom, String cognoms, String carrec) {
+        this.id = id;
+        this.nom = nom;
+        this.cognoms = cognoms;
+        this.carrec = carrec;
+    }
+
+    public short getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCognoms() {
+        return cognoms;
+    }
+    public String getCarrec() {
+        return carrec;
+    }
+
+    public void setCarrec(String carrec) {
+        this.carrec = carrec;
+    }
+
+    public short getPrioritat() {
+        return prioritat;
+    }
+
+    public void setPrioritat(short prioritat) {
+        this.prioritat = prioritat;
+    }
     
 }

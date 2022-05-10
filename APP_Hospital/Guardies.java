986e3eb66fe5b72723cdbd57ae7738b11599b795
Nil Pujol <nilpujolporta@gmail.com>
2022-05-10 13:36:05 +0200

    setters, getters i funcions bàsiques de guardies

--- APP_Hospital/Guardies.java
+++ APP_Hospital/Guardies.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Guardies {
-    private int id;
+    private int id = -1;
     List<Treballador> trApuntats = new ArrayList<Treballador>();
     private Dia dia;
     private Categoria cat;
     private Torn torn;
     private Zona zona;
+    private Treballador principal;
 
     public Guardies(int id, List<Treballador> trApuntats, Dia dia, Categoria cat, Torn torn, Zona zona) {
         this.id = id;
@@ -18,6 +19,33 @@ public class Guardies {
         this.cat = cat;
         this.torn = torn;
         this.zona = zona;
+    }
+
+    public void setId(int id) {
+        if (this.id != -1 || id <= 0) {
+            return;
+        }
+        this.id = id;
+    }
+
+    public List<Treballador> getTrApuntats() {
+        return trApuntats;
+    }
+
+    public Dia getDia() {
+        return dia;
+    }
+
+    public Categoria getCat() {
+        return cat;
+    }
+
+    public Torn getTorn() {
+        return torn;
+    }
+
+    public Zona getZona() {
+        return zona;
     }
 
     public Guardies(int id, Dia dia, Categoria cat, Torn torn, Zona zona) {
@@ -40,9 +68,17 @@ public class Guardies {
         return id;
     }
     
-    public void reservarGuardia(Treballador Treballador){
-        TrApuntats.add(Treballador);
+    public boolean reservarGuardia(Treballador Treballador){
+        if (this.trApuntats.contains(Treballador) || this.getCat() != Treballador.getCarrec()){
+            return false;
+        }
+        trApuntats.add(Treballador);
         Treballador.reservarGuardia(this);
+        return true;
+    }
+    public void triaTreballador(){
+        //comprovacions de prioritat
+        //posa el treballador amb mes prioritat a la variable this.principal
     }
 
 }

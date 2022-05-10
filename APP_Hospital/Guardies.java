a83d1efc98a3bc3fee0218ba62061d988e62f834
Nil Pujol <nilpujolporta@gmail.com>
2022-05-10 13:22:32 +0200

    Fer constructors de guardies

--- APP_Hospital/Guardies.java
+++ APP_Hospital/Guardies.java
@@ -5,7 +5,36 @@ import java.util.List;
 
 public class Guardies {
     private int id;
-    List<Treballador> TrApuntats = new ArrayList<Treballador>();
+    List<Treballador> trApuntats = new ArrayList<Treballador>();
+    private Dia dia;
+    private Categoria cat;
+    private Torn torn;
+    private Zona zona;
+
+    public Guardies(int id, List<Treballador> trApuntats, Dia dia, Categoria cat, Torn torn, Zona zona) {
+        this.id = id;
+        this.trApuntats = trApuntats;
+        this.dia = dia;
+        this.cat = cat;
+        this.torn = torn;
+        this.zona = zona;
+    }
+
+    public Guardies(int id, Dia dia, Categoria cat, Torn torn, Zona zona) {
+        this.id = id;
+        this.dia = dia;
+        this.cat = cat;
+        this.torn = torn;
+        this.zona = zona;
+    }
+
+    public Guardies(Dia dia, Categoria cat, Torn torn, Zona zona) {
+        this.dia = dia;
+        this.cat = cat;
+        this.torn = torn;
+        this.zona = zona;
+    }
+
 
     public int getId() {
         return id;

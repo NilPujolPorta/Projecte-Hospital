377032538cce40e6bb568704f1c74884c8f378b4
Nil Pujol <nilpujolporta@gmail.com>
2022-05-20 12:43:31 +0200

    Implementar GUI

--- APP_Hospital/view/gui/main.java
+++ APP_Hospital/view/gui/main.java
@@ -1,18 +1,23 @@
 package APP_Hospital.view.gui;
 
-import java.io.InputStream;
-import java.util.Calendar;
-import java.util.Properties;
-import java.util.Scanner;
-import java.util.Arrays;
-import APP_Hospital.model.business.utils.utils;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class main {
+public class Main extends Application {
 
-    public static void main(String[] args) {
-        utils.loadConfig();
-        calendari.menuOpcions();
-
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
     }
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

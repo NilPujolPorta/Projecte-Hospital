c15d4de72ec6d100f6d9ff4ae61d79a35cbdce8f
Nil Pujol <nilpujolporta@gmail.com>
2022-05-20 12:10:12 +0200

    update fxml controller

--- APP_Hospital/controller/FXMLDocumentController.java
+++ APP_Hospital/controller/FXMLDocumentController.java
@@ -1,45 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package APP_Hospital.controller;
-
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
-/**
- *
- * @author nilp2
- */
-public class FXMLDocumentController implements Initializable {
+public class FXMLDocumentController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
     private Button button;
+
     @FXML
-    private Button btn_desar;
+    void btnSaludarAction(ActionEvent event) {
+
+    }
+
     @FXML
-    private Label nomlable;
-    @FXML
-    private TextField nominput;
-    @FXML
-    private Label dniLable;
-    @FXML
-    private TextField dniinput;
-    @FXML
-    private Label edatLable;
-    @FXML
-    private TextField edatInput;
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        // TODO Auto-generated method stub
-        
+    void initialize() {
+        assert button != null : "fx:id=\"button\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+
     }
 
 }
